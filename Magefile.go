cf78b3a8e5f459502f495d739475ddbed94bfd57
yago yudi <yagofuruta@protonmail.com>
2024-10-31 17:10:56 -0300

    feat: magefile

---
+++ Magefile.go
@@ -0,0 +1,29 @@
+//go:build mage
+
+package main
+
+import "github.com/magefile/mage/sh"
+
+func Build() error {
+	err := sh.RunV("go", "build", "-o", "bin/t1", "./cmd/t1")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Test() error {
+	err := sh.RunV("go", "test", "./...")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Clean() error {
+	err := sh.RunV("rm", "-rf", "bin")
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yagoyudi/criptografia-t1
 
 go 1.22.6
+
+require github.com/magefile/mage v1.15.0
