45f283655c85d789486b8026fff1baacf8e24a94
yago yudi <yagofuruta@protonmail.com>
2024-12-04 08:32:59 -0300

    feat: deploy code completion

--- magefile.go
+++ magefile.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/carolynvs/magex/pkg"
 	"github.com/magefile/mage/sh"
 )
@@ -23,4 +26,29 @@ func Clean() error {
 
 func EnsureMage() error {
 	return pkg.EnsureMage("")
+}
+
+func BashCompletion() error {
+	Build()
+
+	output, err := sh.Output("./bin/t1", "completion", "bash")
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create("/tmp/completion")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(output)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("bash", "-c", "source /tmp/completion")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
